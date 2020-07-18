e848efe25d458d159cc5de8814dd88895558ac32
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-07-18 12:22:12 +0530

    My First Program

---
+++ Hello World/Main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Type of comment
+/* Another type of comment*/
+
+func main() {
+	// declaring name variable
+	var name = "Shreyas"
+	fmt.Println("Hello my name is " + name)
+}
