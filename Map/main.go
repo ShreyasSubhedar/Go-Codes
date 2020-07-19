f2c17808ff595ca67ccedc4e8a98bef7fb2976fc
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-07-19 20:08:28 +0530

    Maps in Go

---
+++ Map/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1st way to initliaze map
+	colors := map[string]string{
+		"red":   "Laal",
+		"green": "Hirwa",
+		"blue":  "Nila",
+		"Pink":  "Gulabi",
+	}
+	colors["Purple"] = "Jambhla"
+	fmt.Println(colors)
+	// 2nd way to initialize map
+	var mob map[string]int64
+	//mob["Shreyas"] = 406268
+	fmt.Println(mob)
+	// 3rd way to
+	colu := make(map[string]string)
+	colu["kala"] = "black"
+	fmt.Println(colu)
+
+	// deleting the map key value pair
+	delete(colors, "green")
+	fmt.Println(colors)
+
+}
