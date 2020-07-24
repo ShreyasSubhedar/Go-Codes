1b845810765de0b9137adf9e94eccdc6fdbd4474
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-07-24 11:22:49 +0530

    Another Method to read the Body

--- Cards/http/main.go
+++ Cards/http/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	resp, _ := http.Get("http://google.com/")
-	b := make([]byte, 99999)
-	resp.Body.Read(b)
-	fmt.Println(string(b))
+	//b := make([]byte, 99999)
+	//resp.Body.Read(b)
+	//fmt.Println(string(b))
+	// Another method of reading the body
+	io.Copy(os.Stdout, resp.Body)
+
 }
